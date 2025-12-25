dc4680cac8d64d61503d2d47f881ba8c308fa6ce
MUHITRAM <131799906+MUHITRAM@users.noreply.github.com>
2025-12-25 19:37:04 +0530

    Implement BaseTest class for Selenium tests

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,1 +1,28 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import utils.DriverFactory;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverFactory.createDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
 
