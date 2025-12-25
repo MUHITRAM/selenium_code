71dc23fec6b30acc4d924383150b5abb1159ec2f
MUHITRAM <131799906+MUHITRAM@users.noreply.github.com>
2025-12-25 19:21:53 +0530

    Refactor LoginTest to use TestNG annotations

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,14 +2,35 @@ package tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class LoginTest {
 
-    public static void main(String[] args) {
+    WebDriver driver;
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://example.com");
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/login");
+    }
+
+    @Test
+    public void validLoginTest() {
+        driver.findElement(org.openqa.selenium.By.id("username"))
+              .sendKeys("tomsmith");
+
+        driver.findElement(org.openqa.selenium.By.id("password"))
+              .sendKeys("SuperSecretPassword!");
+
+        driver.findElement(org.openqa.selenium.By.cssSelector("button[type='submit']"))
+              .click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
         driver.quit();
     }
 }
