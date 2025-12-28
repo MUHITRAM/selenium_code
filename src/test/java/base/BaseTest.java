a9ca7e4b5bcddc4c140a3c69cfe31ac661badd5c
MUHITRAM <131799906+MUHITRAM@users.noreply.github.com>
2025-12-28 12:01:08 +0530

    Refactor BaseTest to use ChromeDriver with options
    Removed WebDriverWait and DriverFactory usage. Updated driver initialization to use ChromeDriver with options for headless execution.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,28 +1,24 @@
-package base;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeMethod;
-import utils.DriverFactory;
-
-import java.time.Duration;
 
 public class BaseTest {
 
     protected WebDriver driver;
-    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
-        driver = DriverFactory.createDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.get("https://the-internet.herokuapp.com/login");
-    }
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
+        ChromeOptions options = new ChromeOptions();
+
+        // REQUIRED for GitHub Actions
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
     }
 }
-
